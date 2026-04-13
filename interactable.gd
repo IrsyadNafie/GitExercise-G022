15e7a48f9f3c1573c773da3bc5ff225057a746d1
Irsyad Nafi'e <irsyad050207@gmail.com>
2026-04-13 12:20:26 +0800

    Update1

---
+++ interactable.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var player_near = false
+
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+
+func _process(delta):
+	if player_near and Input.is_action_just_pressed("interact"):
+		print("You interacted with the object!")
+
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		player_near = true
+
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		player_near = false
