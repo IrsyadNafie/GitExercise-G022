f6d7ce508b93182fa8ef1ae17545995616adeef9
Rivalino <rivalinogeo@gmail.com>
2026-04-20 00:35:28 +0800

    test

--- control.gd
+++ control.gd
@@ -1,7 +1,8 @@
-extends Control
+extends Control 
 
-@onready var hp_bar = $HPBar
-@onready var sp_bar = $SPBar
+@onready var hp_bar = $PlayerPanel/HPBar
+@onready var sp_bar = $PlayerPanel/SPBar
+
 func update_hp(current_hp, max_hp):
 	hp_bar.max_value = max_hp
 	hp_bar.value = current_hp
@@ -11,8 +12,9 @@ func update_sp(current_sp, max_sp):
 	sp_bar.value = current_sp
 
 func _input(event):
-	if event.is_action_pressed("ui_up"):
-		update_hp(hp_bar.value - 1, hp_bar.max_value)
-	
-	if event.is_action_pressed("ui_down"):
-		update_sp(sp_bar.value - 1, sp_bar.max_value)
+	if event is InputEventKey and event.pressed and not event.is_echo():
+		if event.physical_keycode == KEY_W:
+			update_hp(hp_bar.value - 1, hp_bar.max_value)
+
+		if event.physical_keycode == KEY_S:
+			update_sp(sp_bar.value - 1, sp_bar.max_value)
