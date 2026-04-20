bc9640bcc77e2451f4d4597046b1ab768349e3a0
Rivalino <rivalinogeo@gmail.com>
2026-04-20 12:00:53 +0800

    UI

--- control.gd
+++ control.gd
@@ -1,7 +1,8 @@
-extends Control 
+extends Control
 
 @onready var hp_bar = $PlayerPanel/HPBar
 @onready var sp_bar = $PlayerPanel/SPBar
+@onready var exp_bar = $PlayerPanel/EXP
 
 func update_hp(current_hp, max_hp):
 	hp_bar.max_value = max_hp
@@ -11,10 +12,20 @@ func update_sp(current_sp, max_sp):
 	sp_bar.max_value = max_sp
 	sp_bar.value = current_sp
 
+func update_xp(current_xp, max_xp):
+	exp_bar.max_value = max_xp
+	if current_xp >= max_xp:
+		exp_bar.value = current_xp - max_xp
+	else:
+		exp_bar.value = current_xp
+
 func _input(event):
 	if event is InputEventKey and event.pressed and not event.is_echo():
 		if event.physical_keycode == KEY_W:
 			update_hp(hp_bar.value - 1, hp_bar.max_value)
 
-		if event.physical_keycode == KEY_S:
+		elif event.physical_keycode == KEY_S:
 			update_sp(sp_bar.value - 1, sp_bar.max_value)
+
+		elif event.physical_keycode == KEY_A:
+			update_xp(exp_bar.value + 1, exp_bar.max_value)
